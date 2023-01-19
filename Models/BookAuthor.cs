26238e5fba28327b55c84b4bdc6afbbef4764f98
Piotr Jastrzębski <hawciu@gmail.com>
2023-01-19 01:42:10 +0100

    Delete Models directory

--- Models/BookAuthor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace patronage_restful.Models
-{
-    public class BookAuthor
-    {
-        public int Id { get; set; }
-        public Book Book { get; set; } = null!;
-        public Author Author { get; set; } = null!;
-    }
-}
