e8c13bd18dec8e9e7d59a6ca35aa3b9c4b91b541
Piotr Jastrzębski <hawciu@gmail.com>
2023-01-19 01:44:11 +0100

    Add files via upload

---
+++ patronage_restful/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace patronage_restful.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public String Title { get; set; } = null!;
+        public String Description { get; set; } = null!;
+
+        [Column(TypeName = "decimal(5,2)")]
+        public Decimal Rating { get; set; }
+        public String ISBN { get; set; } = null!;
+        public DateTime PublicationDate { get; set; }
+    }
+}
